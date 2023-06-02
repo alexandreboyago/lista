464bbd96cbf5e8716f895dfea3d4c91f78bb436e
alexandreboyago <134931522+alexandreboyago@users.noreply.github.com>
2023-06-02 11:33:48 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -2,5 +2,8 @@
   {"id": 1, "Nome": "José", "Idade": 18},
   {"id": 2, "Nome": "Chico", "Idade": 19},
   {"id": 3, "Nome": "Tonho", "Idade": 20},
-  {"id": 4, "Nome": "Joaquim", "Idade": 50}
+  {"id": 4, "Nome": "Joaquim", "Idade": 50},
+  {"id": 5, "Nome": "Arthur", "Idade": 6},
+  {"id": 6, "Nome": "Matheus", "Idade": 16},
+  {"id": 7, "Nome": "Jurandir", "Idade": 63},
 ]
